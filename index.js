1fec79a81ace772d4d47d282cf7bd96e4ead7d89
ACVis <theebolivianessence@gmail.com>
2019-12-02 00:57:04 -0500

    -Normalize uses temp files -Clean up comments

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const stream = require("stream");
 
 const unzipper = require("unzipper");
 const ffmpegNormalize = require("ffmpeg-normalize");
+const uniqid = require("uniqid");
 
 const ffmpeg_static = require("ffmpeg-static");
 const ffprobe_static = require("ffprobe-static");
@@ -37,28 +38,18 @@ var command = ffmpeg();
 const prog = require("caporal");
 prog
   .version("1.0.0")
-  // you specify arguments using .argument()
-  // 'app' is required, 'env' is optional
-  // .command('deploy', 'Deploy an application')
-  .argument("<source>", "App to deploy")
-  .argument("[destination]", "Environment to deploy on")
-  // you specify options using .option()
-  // if --tail is passed, its value is required
+  .argument("<source>", "Source of powerpoint file")
+  .argument("[destination]", "Output directory")
   .option(
     "-n, --normalize",
-    "Tail <lines> lines of logs after deploy",
+    `Perform two pass audio normalization.`,
     prog.BOOL
   )
   .action(function(args, options, logger) {
     // args and options are objects
     // args = {"app": "myapp", "env": "production"}
     // options = {"tail" : 100}
-    console.log(args);
-    console.log(options);
-    logger.info(options);
-    // logger.warn(options);
-    // logger.error(options);
-    // logger.debug(options);
+
     if (args.source) {
       const src = path.resolve(args.source);
       const destDefault = path.resolve(__dirname, path.parse(args.source).name);
@@ -66,45 +57,24 @@ prog
       const dest = !!args.destination
         ? path.resolve(args.destination)
         : destDefault;
-      // extract(args.source, dest);
-      
+
       if (options.normalize) {
-        console.log("normalize");
-        
-        const test = path.join(__dirname, "ch07.3/media1.wav");
-        console.log(test);
+        // console.log("normalize");
         extract(args.source, dest, true);
+
+        // const test = path.join(__dirname, "ch07.3/media1.wav");
+        // console.log(test);
         //One pass normalization:
         // ffmpeg -i input.wav -filter:a loudnorm output.wav
         //INCOMPLETE Attempt:
         // ffmpeg(test).output("outputfile.wav").on('end', function() {
-        //   console.log("FINISHED");
-        // }).run()
-        //See: https://superuser.com/questions/323119/how-can-i-normalize-audio-using-ffmpeg
-
-        // normalize({
-        //   input: test,
-        //   output: "output.mp4",
-        //   loudness: {
-        //     normalization: "ebuR128",
-        //     target: {
-        //       input_i: -23,
-        //       input_lra: 7.0,
-        //       input_tp: -2.0
-        //     }
-        //   },
-        //   verbose: true
-        // })
-        //   .then(normalized => {
-        //     // Normalized
-        //   })
-        //   .catch(error => {
-        //     // Some error happened
-        //   });
+          //   console.log("FINISHED");
+          // }).run()
+          //See: https://superuser.com/questions/323119/how-can-i-normalize-audio-using-ffmpeg
+        } else {
+          extract(args.source, dest, false);
       }
-    } else {
-      logger.warn("Please specify a source");
-    }
+    } 
   });
 
 prog.parse(process.argv);
@@ -113,7 +83,6 @@ prog.parse(process.argv);
 //UNZIP
 //==================//
 function extract(source, destination, normalize = false) {
-  // const pptPath = path.resolve(__dirname, "ch07.3.pptx");
   const pptPath = source;
   const root = path.resolve(__dirname);
   // const outputDir = path.resolve(path.join(__dirname, "/output"));
@@ -129,7 +98,10 @@ function extract(source, destination, normalize = false) {
         if (path.extname(fileName) === ".wav") {
           // console.log(type, size, fileName)
           const fullFilePath = path.join(outputDir, path.parse(fileName).base);
-          const fullFilePath_normalized = path.join(outputDir, path.parse(fileName).name + "_normalized" + path.parse(fileName).ext);
+          const fullFilePath_normalized = path.join(
+            outputDir,
+            path.parse(fileName).name + "_normalized" + path.parse(fileName).ext
+          );
           console.log(fullFilePath_normalized);
 
           console.log(outputDir);
@@ -139,27 +111,46 @@ function extract(source, destination, normalize = false) {
           // entry.pipe(fs.createWriteStream(outputDir));
           const content = await entry.buffer();
           // await fs.writeFile(outputDir, content);
-          fs.writeFileSync(fullFilePath_normalized, content);
           if (normalize) {
-          ffmpegNormalize({
-            input: fullFilePath_normalized,
-            output: fullFilePath_normalized,
-            loudness: {
-              normalization: "ebuR128",
-              target: {
-                input_i: -23,
-                input_lra: 7.0,
-                input_tp: -2.0
-              }
-            },
-            verbose: false
-          })
+            const uid = uniqid();
+            const fullFilePath_temp = path.join(
+              outputDir,
+              path.parse(fileName).name +
+                uid +
+                path.parse(fileName).ext
+            );
+            fs.writeFileSync(fullFilePath_temp, content);
+            ffmpegNormalize({
+              input: fullFilePath_temp,
+              output: fullFilePath_normalized,
+              loudness: {
+                normalization: "ebuR128",
+                target: {
+                  input_i: -23,
+                  input_lra: 7.0,
+                  input_tp: -2.0
+                }
+              },
+              verbose: false
+            })
             .then(normalized => {
-              // Normalized
+              //Delete non-normalized version (async)
+              fs.unlink(fullFilePath_temp, err => {
+                if (err) {
+                  console.error("Error while deleting temp file: " + fullFilePath_temp, err);
+                  return;
+                }
+                //file removed
+              });
             })
             .catch(error => {
               // Some error happened
+              console.log("Error during normalization: ", error);
+              
             });
+          } else {
+            //Write non-normalized file
+            fs.writeFileSync(fullFilePath, content);
           }
         } else {
           entry.autodrain();
