16fe8b02d108ae43eea6f12750b6a99e05fba947
ACVis <theebolivianessence@gmail.com>
2019-12-02 00:20:09 -0500

    -Add normalization

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ const path = require("path");
 const stream = require("stream");
 
 const unzipper = require("unzipper");
-const commander = require("commander");
-const ffmpeg = require("ffmpeg-static");
+const ffmpegNormalize = require("ffmpeg-normalize");
+
+const ffmpeg_static = require("ffmpeg-static");
+const ffprobe_static = require("ffprobe-static");
 // console.log(ffmpeg.path);
 const NormalizeVolume = require("normalize-volume");
 
-let program = new commander.Command();
-// let program = require("commander");
+// const FfmpegCommand = require("fluent-ffmpeg");
+// FfmpegCommand.setFfmpegPath(ffmpeg_static);
+// FfmpegCommand.setFfprobePath(ffprobe_static);
+// const ffmpeg_command = new FfmpegCommand();
+var ffmpeg = require("fluent-ffmpeg");
+ffmpeg.setFfmpegPath(ffmpeg_static.path);
+ffmpeg.setFfprobePath(ffprobe_static.path);
+var command = ffmpeg();
+
 // console.log(__dirname);
 //==================//
 //list root directory
@@ -51,17 +60,47 @@ prog
     // logger.error(options);
     // logger.debug(options);
     if (args.source) {
-      console.log("extract");
       const src = path.resolve(args.source);
       const destDefault = path.resolve(__dirname, path.parse(args.source).name);
       //If no destination provided, set it to name of file, without extension
       const dest = !!args.destination
         ? path.resolve(args.destination)
         : destDefault;
-      extract(args.source, dest);
-
+      // extract(args.source, dest);
+      
       if (options.normalize) {
-        //
+        console.log("normalize");
+        
+        const test = path.join(__dirname, "ch07.3/media1.wav");
+        console.log(test);
+        extract(args.source, dest, true);
+        //One pass normalization:
+        // ffmpeg -i input.wav -filter:a loudnorm output.wav
+        //INCOMPLETE Attempt:
+        // ffmpeg(test).output("outputfile.wav").on('end', function() {
+        //   console.log("FINISHED");
+        // }).run()
+        //See: https://superuser.com/questions/323119/how-can-i-normalize-audio-using-ffmpeg
+
+        // normalize({
+        //   input: test,
+        //   output: "output.mp4",
+        //   loudness: {
+        //     normalization: "ebuR128",
+        //     target: {
+        //       input_i: -23,
+        //       input_lra: 7.0,
+        //       input_tp: -2.0
+        //     }
+        //   },
+        //   verbose: true
+        // })
+        //   .then(normalized => {
+        //     // Normalized
+        //   })
+        //   .catch(error => {
+        //     // Some error happened
+        //   });
       }
     } else {
       logger.warn("Please specify a source");
@@ -69,66 +108,6 @@ prog
   });
 
 prog.parse(process.argv);
-
-//==================//
-//Commander version
-//==================//
-// program
-//   .option("-n, --normaliz", "output extra debugging")
-
-// program.parse(process.argv);
-
-// if (program.normalize) console.log(program.opts());
-
-// .action((args, options) => {
-// console.log(args)
-// console.log(options)
-// })
-// .arguments("<source> [destination]")
-// .action((source, destination = path.parse(source).name, cmdObj) => {
-//   // console.log({program});
-//   console.log("OPTIONS: ", program.opts())
-//   // console.log(source, destination);
-//   src = source;
-//   dest = destination;
-// })
-// .parse(process.argv);
-
-// console.log("OPTIONS: ", program.options.n)
-// console.log({program})
-// if(typeof src !== 'undefined') {
-//   src = path.resolve(src);
-//   console.log("SOURCE     : ", src)
-//   dest = path.resolve(dest)
-//   console.log("DESTINATION: ", dest);
-//   if(program.normalize) {
-//     console.log("NORMALIZE!!!!!!!!!!!!!!!!!!!!!")
-//   } else {
-//     // extract(src, dest);
-//     console.log("EXTRACT!!!!")
-//   }
-// }
-
-// console.log(program.normalize == true)
-
-//  program = require("commander");
-
-// program
-//   .version("0.1.0")
-//   .arguments("<cmd> [env]")
-//   .action(function(cmd, env) {
-//     cmdValue = cmd;
-//     envValue = env;
-//   });
-
-// program.parse(process.argv);
-
-// if (typeof cmdValue === "undefined") {
-//   console.error("no command given!");
-//   process.exit(1);
-// }
-// console.log("command:", cmdValue);
-// console.log("environment:", envValue || "no environment given");
 
 //==================//
 //UNZIP
@@ -150,7 +129,8 @@ function extract(source, destination, normalize = false) {
         if (path.extname(fileName) === ".wav") {
           // console.log(type, size, fileName)
           const fullFilePath = path.join(outputDir, path.parse(fileName).base);
-          console.log(fullFilePath);
+          const fullFilePath_normalized = path.join(outputDir, path.parse(fileName).name + "_normalized" + path.parse(fileName).ext);
+          console.log(fullFilePath_normalized);
 
           console.log(outputDir);
           if (!fs.existsSync(outputDir)) {
@@ -159,23 +139,28 @@ function extract(source, destination, normalize = false) {
           // entry.pipe(fs.createWriteStream(outputDir));
           const content = await entry.buffer();
           // await fs.writeFile(outputDir, content);
-          fs.writeFileSync(fullFilePath, content);
-          // if (normalize) {
-          //   let options = {
-          //     normalize: true,
-          //     waveform: { width: 1400, height: 225 },
-          //     ffmpeg_bin: ffmpeg
-          //     // convert_bin: "convert.exe"
-          //   };
-
-          //   NormalizeVolume(fullFilePath, fullFilePath + "n", options)
-          //     .then(result => {
-          //       console.log(result);
-          //     })
-          //     .catch(err => {
-          //       console.log(err);
-          //     });
-          // }
+          fs.writeFileSync(fullFilePath_normalized, content);
+          if (normalize) {
+          ffmpegNormalize({
+            input: fullFilePath_normalized,
+            output: fullFilePath_normalized,
+            loudness: {
+              normalization: "ebuR128",
+              target: {
+                input_i: -23,
+                input_lra: 7.0,
+                input_tp: -2.0
+              }
+            },
+            verbose: false
+          })
+            .then(normalized => {
+              // Normalized
+            })
+            .catch(error => {
+              // Some error happened
+            });
+          }
         } else {
           entry.autodrain();
         }
